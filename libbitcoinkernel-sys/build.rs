d31e63b6d4e2f7b5d0427d0dd59d12f6eba14672
alexanderwiederin <alex@wiederin.xyz>
2025-08-27 10:42:25 +0200

    test bcrypt

--- libbitcoinkernel-sys/build.rs
+++ libbitcoinkernel-sys/build.rs
@@ -96,7 +96,6 @@ fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if target_os == "windows" {
-        println!("cargo:rustc-link-lib=bcrypt");
         println!("cargo:rustc-link-lib=shell32");
     }
 
