2566eb37c67ab5225353994f233c3df99aaf5324
alexanderwiederin <alex@wiederin.xyz>
2025-09-11 14:48:08 +0200

    refactor(core): extract verification to verify.rs
    Move verify function, ScriptVerificationFlags and ScriptVerifyError to
    dedicated module.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -6,6 +6,7 @@
 pub mod block;
 pub mod script;
 pub mod transaction;
+pub mod verify;
 
 pub use block::{
     Block, BlockHash, BlockSpentOutputs, BlockTreeEntry, Coin, TransactionSpentOutputs,
@@ -16,3 +17,12 @@ pub use transaction::{Transaction, TxOut};
 pub use block::{BlockSpentOutputsExt, CoinExt, TransactionSpentOutputsExt};
 pub use script::ScriptPubkeyExt;
 pub use transaction::{TransactionExt, TxOutExt};
+
+pub use verify::{verify, ScriptVerifyError, ScriptVerifyStatus};
+
+pub mod verify_flags {
+    pub use super::verify::{
+        VERIFY_ALL, VERIFY_ALL_PRE_TAPROOT, VERIFY_CHECKLOCKTIMEVERIFY, VERIFY_CHECKSEQUENCEVERIFY,
+        VERIFY_DERSIG, VERIFY_NONE, VERIFY_NULLDUMMY, VERIFY_P2SH, VERIFY_TAPROOT, VERIFY_WITNESS,
+    };
+}
