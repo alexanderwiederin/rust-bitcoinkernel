1905961ccc620a9239b658831834561966ac7f61
alexanderwiederin <alex@wiederin.xyz>
2025-10-30 13:01:42 +0100

    feat(fuzz): verify deterministi block serialization round-trips

--- fuzz/fuzz_targets/fuzz_target_block.rs
+++ fuzz/fuzz_targets/fuzz_target_block.rs
@@ -5,7 +5,17 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(block) = Block::try_from(data) {
-        let block_serialized: Vec<u8> = block.try_into().unwrap();
-        assert!(data.len() >= block_serialized.len());
+        let serialized: Vec<u8> = block
+            .consensus_encode()
+            .expect("Valid block should serialize");
+
+        let reparsed =
+            Block::try_from(serialized.as_slice()).expect("Serialized block should deserialize");
+
+        let reserialized: Vec<u8> = reparsed
+            .consensus_encode()
+            .expect("Reparsed block should serialize");
+
+        assert_eq!(serialized, reserialized, "Non-deterministic serialization");
     }
 });
