57405a61e213147d79d4c80e0fcd519ce1440df4
Yeji Han <yeji.hvn@gmail.com>
2026-03-05 13:42:53 +0900

    script_debug: expose decoded opcode and op_count in callback

--- libbitcoinkernel-sys/bitcoin/src/script/interpreter.cpp
+++ libbitcoinkernel-sys/bitcoin/src/script/interpreter.cpp
@@ -444,11 +444,11 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
             // Read instruction
             //
             if (!script.GetOp(pc, opcode, vchPushValue)) {
-                DEBUG_SCRIPT(stack, script, opcode_pos, altstack, fExec);
+                DEBUG_SCRIPT(stack, script, opcode_pos, altstack, fExec, static_cast<uint8_t>(OP_INVALIDOPCODE), nOpCount);
                 return set_error(serror, SCRIPT_ERR_BAD_OPCODE);
             }
 
-            DEBUG_SCRIPT(stack, script, opcode_pos, altstack, fExec);
+            DEBUG_SCRIPT(stack, script, opcode_pos, altstack, fExec, static_cast<uint8_t>(opcode), nOpCount);
 
             if (vchPushValue.size() > MAX_SCRIPT_ELEMENT_SIZE)
                 return set_error(serror, SCRIPT_ERR_PUSH_SIZE);
@@ -1238,7 +1238,7 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
         return set_error(serror, SCRIPT_ERR_UNKNOWN_ERROR);
     }
 
-    DEBUG_SCRIPT(stack, script, opcode_pos, altstack, vfExec.all_true());
+    DEBUG_SCRIPT(stack, script, opcode_pos, altstack, vfExec.all_true(), static_cast<uint8_t>(OP_INVALIDOPCODE), nOpCount);
 
     if (!vfExec.empty())
         return set_error(serror, SCRIPT_ERR_UNBALANCED_CONDITIONAL);
